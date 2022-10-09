043ff3f30c856833d862ef4a8aef0f00aa1348b2
YOUR NAMEAlena <alena.yefymenko@gmail.com>
2022-10-09 19:30:07 +0300

    Initial commit

---
+++ src/main/java/UniversityCommandLine.java
@@ -0,0 +1,77 @@
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UniversityCommandLine {
+    public static void main(String[] args) {
+        printCommands();
+        readCommands();
+    }
+
+    public static void printCommands() {
+        System.out.println("Welcome to the University!");
+        System.out.println();
+        // System.out.println("1. Add Student");
+        // System.out.println("2. Vew All students");
+        // System.out.println("3. Vew Single Student info");
+        //System.out.println("4. Enroll student in to the course");
+        // System.out.println("5. View student Exams");
+        // System.out.println("6. Delete student");
+        System.out.println("7. Add Course");
+        System.out.println("8. View All Courses");
+        System.out.println("9. View Single Course info");
+        System.out.println(" ---------------------- ");
+        System.out.println("Choose the number for activity You would like to perform: ");
+    }
+
+    public static void readCommands() {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        switch (i) {
+            case 7 -> addCourse();
+            case 8 -> viewAllCourses();
+            case 9 -> viewSingleCourseInfo();
+        }
+    }
+
+    private static void viewAllCourses() {
+        ArrayList<Course> courses;
+        try {
+            courses = UniversityDB.getCourses();
+            for (Course c : courses) {
+                System.out.println(c);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void addCourse() {
+        System.out.println("Enter course title:");
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        System.out.println("Enter duration (hours): ");
+        int duration = scanner.nextInt();
+        try {
+            UniversityDB.addCourse(title, duration);
+            System.out.println("New course has been added successfully!");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void viewSingleCourseInfo() {
+        System.out.println("Enter course title:");
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        Course course;
+        try {
+          course =  UniversityDB.getCourseByTitle(title);
+          System.out.println(course);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+
---
+++ src/main/java/UniversityDB.java
@@ -0,0 +1,66 @@
+import java.awt.*;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class UniversityDB {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    /*ArrayList<Course> c = getCourses();
+    for(Course course: c){
+        System.out.println(course);
+    }*/
+        //System.out.println(getCourseByTitle("Web technology"));
+
+    }
+
+    public static ArrayList<Course> getCourses() throws SQLException, ClassNotFoundException {
+        Connection con = getConnection();
+        String sql = "select * from courses";
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+
+        ArrayList<Course> courses = new ArrayList<>();
+        while (rs.next()) {
+            int id = rs.getInt("ID");
+            String courseTitle = rs.getNString("Title");
+            int courseDur = rs.getInt("Duration");
+            Course course = new Course(id, courseTitle, courseDur);
+            courses.add(course);
+        }
+        con.close();
+        return courses;
+    }
+
+    public static Course getCourseByTitle(String title) throws SQLException, ClassNotFoundException {
+        Connection con = getConnection();
+        String sql = "select * from courses where title = ? ";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setString(1, title);
+        ResultSet rs = stmt.executeQuery();
+        Course course = null;
+        if (rs.next()) {
+            int courseID = rs.getInt("ID");
+            String courseTitle = rs.getString("Title");
+            int courseDur = rs.getInt("Duration");
+            course = new Course(courseID, courseTitle, courseDur);
+        }
+        con.close();
+        return course;
+    }
+
+    public static void addCourse(String title, int duration ) throws SQLException, ClassNotFoundException{
+        Connection con = getConnection();
+        String sql = "INSERT INTO courses (Title, Duration) values (?,?)";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setString(1,title);
+        stmt.setInt(2,duration);
+        stmt.executeUpdate();
+        con.close();
+    }
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/university", "root", "root");
+        return con;
+    }
+
+}
